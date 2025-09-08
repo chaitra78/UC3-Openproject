1ec993d7a689c9efc6ec9f40cd438ea0a1370f9d
Chaitra N R <chaitra.nr@hcltech.com>
2025-09-08 22:51:57 +0530

    Commit9

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -2,12 +2,20 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+
 resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.cidr_block
+  for_each = { for idx, cidr in var.cidr_block : idx => cidr }
+
+  cidr_block        = each.value
+  vpc_id            = var.vpc_id
+  availability_zone = var.availability_zones[each.key]  # Optional, if you're mapping AZs
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${each.key}"
+  }
 }
+
 
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.main.id
