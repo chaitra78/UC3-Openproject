44c60528c4d37be4822a8e983d813665803dba07
Chaitra N R <chaitra.nr@hcltech.com>
2025-09-09 10:47:16 +0530

    commit13

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,12 +1,13 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
+*/
 output "public_subnet_id" {
   value = aws_subnet.public.id
 }
+*/
 
-output "public_subnet_ids" {
+output "public_subnet_id" {
   value = aws_subnet.public[*].id
 }
 
